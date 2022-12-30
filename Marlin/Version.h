c8dbaadd1ebe707e9f80f086c9beacf5f0a732b6
Matthew Coon <mmcoon@gmail.com>
2022-12-29 20:00:28 -0500

    2.1.3.7 version fix

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.1.3.8"
+#define SHORT_BUILD_VERSION "2.1.3.7"
 
 /**
  * Verbose version identifier which should contain a reference to the location
