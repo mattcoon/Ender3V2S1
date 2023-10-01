c8b6ef5307e3b2e4503d78a55cec9f5573d67a05
Matthew Coon <66556546+mattcoon@users.noreply.github.com>
2023-10-01 18:25:22 -0400

    change dimensions of CNC

--- configurations/CNC/Configuration.h
+++ configurations/CNC/Configuration.h
@@ -1909,7 +1909,7 @@ Flex mesh size
 
 // The size of the printable area
 #define X_BED_SIZE 1220 // 200
-#define Y_BED_SIZE 900 // 200
+#define Y_BED_SIZE 1200 // 200
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0  // MRiscoC Stock physical limit
@@ -1917,7 +1917,7 @@ Flex mesh size
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 90
+#define Z_MAX_POS 80
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
