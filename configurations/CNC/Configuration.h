86f4fda0a02dcd24bfdb84b632bae5cdd9acab77
Matthew Coon <66556546+mattcoon@users.noreply.github.com>
2023-12-29 22:28:02 -0500

    update configurations

--- configurations/CNC/Configuration.h
+++ configurations/CNC/Configuration.h
@@ -1767,8 +1767,8 @@ Flex mesh size
 #define Z_PROBE_LOW_POINT          -2 // Farthest distance below the trigger-point to go before stopping
 
 // For M851 give a range for adjusting the Z probe offset
-#define Z_PROBE_OFFSET_RANGE_MIN -20
-#define Z_PROBE_OFFSET_RANGE_MAX 20
+#define PROBE_OFFSET_RANGE_MIN -20
+#define PROBE_OFFSET_RANGE_MAX 20
 
 // Enable the M48 repeatability test to test probe accuracy
 //#define Z_MIN_PROBE_REPEATABILITY_TEST
